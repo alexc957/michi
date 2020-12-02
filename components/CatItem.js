dba614d32ee3fcf2d3c18716d64e44f4aa94d316
alexc957 <alexcoronel1995@gmail.com>
2020-12-02 09:36:15 -0500

    header and footer added

--- components/CatItem.js
+++ components/CatItem.js
@@ -6,9 +6,9 @@ export default function CatItem({id, path}){
 
     return (
         <Zoom top>
-        <Link href={`/michi/${id}`}>
-          <a><img src={path}   className={styles.photo} /></a>
-        </Link>
+          <Link href={`/michi/${id}`}>
+            <a><img src={path}   className={styles.photo} /></a>
+          </Link>
         </Zoom>
     )
 }
--- pages/about.js
+++ pages/about.js
@@ -4,7 +4,11 @@ export default function About(){
 
     return (
         <Layout>
-            <p>About</p>
+            <div style={{width: "100%", height: "514px", marginBottom: "25px"}}>
+
+               <p style={{textAlign: 'center', paddingTop: '50px'}}> this is a web site to show pictures about my cat and to learn nextjs with react.  </p>   
+
+            </div>
         </Layout>
         
     )
